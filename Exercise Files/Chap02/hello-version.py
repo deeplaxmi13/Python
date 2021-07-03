32b0a70b6612a06df6de490df90a25f660802a03
Deeplaxmi <lambture.d.p.10@gmail.com>
2021-07-03 09:20:37 +0530

    Initial commit

---
+++ Exercise Files/Chap02/hello-version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#  shebang line ->  ! -called as bang and together it is called shebang
+# Copyright 2009-2017 BHG http://bw.org/
+
+import platform
+# tells the python interpreter to import module from library. Platform module is used to display python version
+
+print('This is python version {}'.format(platform.python_version()))
+
